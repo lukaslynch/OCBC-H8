8d85038f9bf96e163f87300d6cc35fdece4b56de
lukaslynch <lukaslynch98@gmail.com>
2021-10-16 17:35:37 +0700

    OCBC H8

---
+++ Sesi 10/TodoAppWithJWT/Data/ApiDbContext.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using TodoAppWithJWT.Models;
+using TodoAppWithJWT.Data;
+using Microsoft.EntityFrameworkCore.Sqlite;
+// using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+
+namespace TodoAppWithJWT.Data
+{
+    public class ApiDbContext : IdentityDbContext
+    {
+        public virtual DbSet<ItemData> Items {get; set;}
+
+        public ApiDbContext(DbContextOptions<ApiDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
