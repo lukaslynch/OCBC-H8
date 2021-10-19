73c8c9f78327d001d441a5d111458094e67f01f4
lukaslynch <lukaslynch98@gmail.com>
2021-10-19 16:24:11 +0700

    OCBC Hacktiv8

---
+++ Sesi 12/UnitTest_Mock/Models/ApiDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnitTest_Mock.Models
+{
+    public partial class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Employee> Employee { get; set; }
+    }
+}
